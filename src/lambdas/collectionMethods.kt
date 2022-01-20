9a9c4404d01c708176d7887482d6e244861fbbb3
eulucasm <lucaspoletis@gmail.com>
2022-01-20 11:07:12 -0300

    feat: conclusion session

--- src/lambdas/collectionMethods.kt
+++ src/lambdas/collectionMethods.kt
@@ -1,5 +1,12 @@
 package lambdas
 
-fun main(args: Array<String>){
+fun main(args: Array<String>) {
+    val values = listOf(24, 5, 10, 4)
+    println(values.filter { it > 5 })
 
+    val names = listOf("Sam", "Fred", "Samuel", "Alice")
+    println(names.first { it.length > 5 })
+
+    println(names.any {it == "Alice"})
+    println(names.all {it.length > 2})
 }
