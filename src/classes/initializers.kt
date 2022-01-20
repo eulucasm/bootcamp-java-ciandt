040e3e064370741988148c4b2bcf89341fe1e259
eulucasm <lucaspoletis@gmail.com>
2022-01-20 15:23:12 -0300

    feat: initializers

--- src/classes/initializers.kt
+++ src/classes/initializers.kt
@@ -1,4 +1,13 @@
 package classes
 
-class initializers {
+fun main(args: Array<String>) {
+    class Person(var firstName:String, var lastName : String) {
+        var fullName: String
+        init {
+            fullName = "$firstName $lastName"
+        }
+    }
+
+    val person = Person("Lucas","Marques")
+    println(person.fullName)
 }
