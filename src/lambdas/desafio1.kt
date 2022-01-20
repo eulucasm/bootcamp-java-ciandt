484fdbf48152ea850dcccb83b2b00fbd279c2162
eulucasm <lucaspoletis@gmail.com>
2022-01-20 10:34:40 -0300

    feat: desafio lambdas

--- src/lambdas/desafio1.kt
+++ src/lambdas/desafio1.kt
@@ -1,5 +1,20 @@
 package lambdas
 
-fun main(args: Array<String>){
+fun main(args: Array<String>) {
 
+    val values = listOf(24, 5, 10, 4)
+    println(values.filter { it > 5 }.sum())
+
+
+    data class Person(val name: String, val age: Int)
+
+    val sam = Person("Sam", 30)
+
+    val (name, age) = sam
+    println("$name is $age years old")
+
+    val names = mapOf<String, Int>("John" to 34, "Fred" to 24, "Sam" to 30, "Suzzy" to 29)
+    for ((name, age) in names) {
+        println("$name is $age years old")
+    }
 }
--- src/lambdas/syntax.kt
+++ src/lambdas/syntax.kt
@@ -1,5 +1,16 @@
 package lambdas
 
-fun main(args: Array<String>){
+fun main(args: Array<String>) {
 
+    fun handleInteger(myInt: Int, operation: (Int) -> Unit) {
+        operation(myInt)
+    }
+
+    handleInteger(5, { println("My Result is ${it * 10}") })
+    handleInteger(5, { _ -> println("My Result is 10") })
+
+    fun printResult(myInt : Int){
+        println("My Int is $myInt")
+    }
+    handleInteger(4, ::printResult)
 }
