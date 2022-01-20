222ac9404b90c663133488571a06985fd28415fe
eulucasm <lucaspoletis@gmail.com>
2022-01-20 10:50:57 -0300

    feat: generics

--- src/lambdas/generics.kt
+++ src/lambdas/generics.kt
@@ -1,5 +1,38 @@
 package lambdas
 
-fun main(args: Array<String>){
+//fun main(args: Array<String>) {
+//
+//    val names = ArrayList<String>()
+//    val myNumbers = ArrayList<Number>()
+//    myNumbers.add(1)
+//    myNumbers.add(2.5)
+//    println(myNumbers)
+//}
 
+interface Repository<T>{
+    fun addItem(item:T)
+    fun deleteItem(item:T)
+}
+class Person(val name: String){}
+
+class PersonRepository : Repository<Person>{
+    override fun addItem(item: Person) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteItem(item: Person) {
+        TODO("Not yet implemented")
+    }
+
+}
+
+fun <T> printItem(item : T){
+    println(item)
+}
+fun <QualquerCoisa> printItem2(item : QualquerCoisa){
+    println(item)
+}
+
+fun main(args: Array<String>) {
+    printItem("teste de generics")
 }
