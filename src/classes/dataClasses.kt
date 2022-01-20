18499ecef1e55ba6a09f66bccc35d5eab849b5b8
eulucasm <lucaspoletis@gmail.com>
2022-01-20 18:53:25 -0300

    feat: Data Classes

--- src/classes/dataClasses.kt
+++ src/classes/dataClasses.kt
@@ -1,4 +1,11 @@
 package classes
 
-class dataClasses {
+fun main(args: Array<String>) {
+    data class Podcast(val title: String, val description: String, val url: String)
+
+    val podcast = Podcast("Adroid Central", "The Premier source for weekly news", "https://umaurl.com.br")
+    val podcast2 = podcast.copy(title = "Developers Backstage")
+
+    val (title, description, url) = podcast2
+    println("title = $title, url = $url")
 }
