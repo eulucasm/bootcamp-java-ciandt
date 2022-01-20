54168292c9961fae7d76e14c41cb9e421159a3dc
eulucasm <lucaspoletis@gmail.com>
2022-01-20 10:20:12 -0300

    feat: Higher-Order Vs Lambdas

--- src/lambdas/higherOrderVsLambdas.kt
+++ src/lambdas/higherOrderVsLambdas.kt
@@ -1,5 +1,14 @@
 package lambdas
 
-fun main(args: Array<String>){
+fun main(args: Array<String>) {
+    val ints = listOf(2, 3, 20, 50)
+    println(ints.filter { it > 10 })
 
+    fun List<Int>.filter(operator: (Int) -> Boolean) {}
+
+    val myStringList = listOf("Sam", "Suzzy", "Jesse", "Ray", "Kevin")
+    val updatedList = myStringList.filter { it.length > 3 }.sortedBy{it}
+    println(updatedList)
+
+    view.onClickListener{ println("in Click")} //exemplo ilustrativo
 }
