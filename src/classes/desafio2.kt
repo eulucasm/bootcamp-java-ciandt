244d1b1f52192e90152a8ad867983d2e6935af8e
eulucasm <lucaspoletis@gmail.com>
2022-01-20 19:02:05 -0300

    feat: challeger 2 and finish module

--- src/classes/desafio2.kt
+++ src/classes/desafio2.kt
@@ -1,4 +1,22 @@
 package classes
+data class Student(
+    val firstName : String,
+    val lastName : String,
+    val grade : Char
+)
+data class Student2(
+    val firstName : String,
+    val lastName : String,
+    val grade : Char = 'A'
+)
 
-class desafio2 {
+fun main(args: Array<String>){
+    var sam = Student("Sam", "Gamgee",'A')
+    println(sam)
+
+    sam = sam.copy(grade = 'B')
+    println(sam)
+
+    val fred = Student2("Fred","Smith")
+    println(fred)
 }
