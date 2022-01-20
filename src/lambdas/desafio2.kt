2c2e7c202f3e079a4e9f09cf49b0f74655a33832
eulucasm <lucaspoletis@gmail.com>
2022-01-20 10:56:19 -0300

    feat: generics challeger

--- src/lambdas/desafio2.kt
+++ src/lambdas/desafio2.kt
@@ -2,4 +2,17 @@ package lambdas
 
 fun main(args: Array<String>){
 
+    fun <T> printItem(item:T){
+        println("My Item is $item")
+    }
+    printItem(1)
+    printItem("Hello")
+
+    fun <T> printList(list : List<T>){
+        for( item in list){
+            println("list item : $item")
+        }
+    }
+    printList((listOf("Lucas", "maria", "joao")))
+
 }
