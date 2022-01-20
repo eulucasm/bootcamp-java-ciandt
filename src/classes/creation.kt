ed280890cbdc3b757a140a953b5a0c6369e9f184
eulucasm <lucaspoletis@gmail.com>
2022-01-20 15:09:30 -0300

    feat: creation

--- src/classes/creation.kt
+++ src/classes/creation.kt
@@ -1,4 +1,13 @@
 package classes
 
-class creation {
+class Person(
+    var firstName: String,
+    var lastName: String = "Marques"
+)
+
+fun main (args: Array<String>){
+    val person = Person("Lucas")
+    val person2 = Person("Lucas", "Maturana")
+    println("Person 1 = ${person.firstName} ${person.lastName}")
+    println("Person 2 = ${person2.firstName} ${person2.lastName}")
 }
