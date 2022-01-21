f2ea306f997557004d1863cf69116818f6d3e31b
eulucasm <lucaspoletis@gmail.com>
2022-01-21 09:29:11 -0300

    feat: desafio 1 - singleton

--- src/moreOnClasses/desafio1.kt
+++ src/moreOnClasses/desafio1.kt
@@ -1,5 +1,28 @@
 package moreOnClasses
 
+data class Student(val firstName: String, val lastName: String)
+
+object StudentRegistry{
+    val allStudents = mutableListOf<Student>()
+
+    fun addStudent(student: Student) {
+        allStudents.add(student)
+    }
+    fun removeStudent(student: Student){
+        allStudents.remove(student)
+    }
+}
+
 fun main(args: Array<String>){
+
+    val lucas = Student("Lucas","Marques")
+    val barbara = Student("Barbara","Maturana")
+
+    StudentRegistry.addStudent(lucas)
+    StudentRegistry.addStudent(barbara)
+
+    StudentRegistry.allStudents.forEach{
+        println("${it.firstName} ${it.lastName}" )
+    }
 
 }
