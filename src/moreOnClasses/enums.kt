4518976e96b3a371cbb8d1fc90dc9309d208afc6
eulucasm <lucaspoletis@gmail.com>
2022-01-21 10:36:11 -0300

    feat: enums

--- src/moreOnClasses/enums.kt
+++ src/moreOnClasses/enums.kt
@@ -1,5 +1,27 @@
 package moreOnClasses
 
-fun main(args: Array<String>){
+enum class Direction {
+    NORTH, SOUTH, WEST, EAST
+}
 
+enum class Color(val r: Int, g: Int, b: Int) {
+    RED( 255, 0, 0),
+    YELLOW(255, 255, 0),
+    GREEN(255, 255, 0)
+}
+
+fun drive(direction: Direction){
+    when (direction){
+        Direction.NORTH -> println("Driving North")
+        Direction.SOUTH -> println("Driving South")
+        Direction.WEST -> println("Driving West")
+        Direction.EAST -> println("Driving East")
+    }
+}
+
+fun main(args: Array<String>) {
+    drive(Direction.NORTH)
+    drive(Direction.EAST)
+
+    println(Direction.WEST.ordinal)
 }
