0f1d79b2461a5721a555c00804b1369a67a5832f
eulucasm <lucaspoletis@gmail.com>
2022-01-21 10:02:06 -0300

    feat: interfaces

--- src/moreOnClasses/companion.kt
+++ src/moreOnClasses/companion.kt
@@ -1,18 +1,18 @@
-package moreOnClasses
-
-class Studentt private constructor(){
-    var firstname : String? = null
-    var lastname : String? = null
-    companion object Loader {
-        fun loadStudent(jsonText: String) : Studentt {
-            val student = Studentt()
-            student.firstname = "Firstname"
-            student.lastName = "Lastname"
-            return student
-        }
-    }
-}
-
-fun main(args: Array<String>){
-    val student = Studentt.loadStudent("JSON Text")
-}
+//package moreOnClasses
+//
+//class Studentt private constructor(){
+//    var firstname : String? = null
+//    var lastname : String? = null
+//    companion object Loader {
+//        fun loadStudent(jsonText: String) : Studentt {
+//            val student = Studentt()
+//            student.firstname = "Firstname"
+//            student.lastName = "Lastname"
+//            return student
+//        }
+//    }
+//}
+//
+//fun main(args: Array<String>){
+//    val student = Studentt.loadStudent("JSON Text")
+//}
--- src/moreOnClasses/interfaces.kt
+++ src/moreOnClasses/interfaces.kt
@@ -1,4 +1,32 @@
 package moreOnClasses
-fun main(args: Array<String>){
 
+interface Animal{
+    fun eat()
+    val numLegs : Int
+}
+
+class Dog : Animal {
+    override val numLegs: Int = 4
+    override fun eat() {
+        println("Dog eating Loudly")
+    }
+}
+
+class Cat : Animal {
+    override val numLegs: Int = 4
+    override fun eat() {
+        println("Cat eating Loudly")
+    }
+    fun meow(){
+        println("meow meow")
+    }
+}
+
+fun main(args: Array<String>){
+    val dog = Dog()
+    val cat = Cat()
+
+    dog.eat()
+    cat.eat()
+    cat.meow()
 }
