af76b8213b7d092e60e6681c31b525e8c66dfd55
eulucasm <lucaspoletis@gmail.com>
2022-01-21 10:12:33 -0300

    feat: desafio 2 - interfaces

--- src/moreOnClasses/desafio2.kt
+++ src/moreOnClasses/desafio2.kt
@@ -1,5 +1,30 @@
 package moreOnClasses
 
+interface Shape {
+    var width : Int
+    var height : Int
+    fun draw()
+}
+
+class Circle(override var width: Int, override var height: Int): Shape{
+    override fun draw() {
+        println("Drawing a Circle")
+    }
+}
+class Line(override var width: Int, override var height: Int): Shape{
+    override fun draw() {
+        println("Drawing a Line")
+    }
+}
+fun printShape(shape: Shape){
+    shape.draw()
+}
+
 fun main(args: Array<String>){
 
+    val circle = Circle(10,10)
+    val line = Line(10,10)
+
+    printShape(circle)
+    printShape(line)
 }
