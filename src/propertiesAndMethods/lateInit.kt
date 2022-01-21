75d04c58b866982e7ba163f9ee6d5c2682c16f28
eulucasm <lucaspoletis@gmail.com>
2022-01-21 17:47:46 -0300

    feat: lateinit

--- src/propertiesAndMethods/lateInit.kt
+++ src/propertiesAndMethods/lateInit.kt
@@ -1,4 +1,15 @@
 package propertiesAndMethods
 
-class lateInit {
+class Personn(var firstName: String, var lastNAme:String){
+    lateinit var fullName : String
+
+    init {
+        fullName = "$firstName $lastNAme"
+    }
+    fun printFullName(){
+        if(!this::fullName.isInitialized){
+            fullName = "$firstName $lastNAme"
+        }
+        println(fullName)
+    }
 }
