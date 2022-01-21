c5f8990400661cab91594171c1975f30372142a4
eulucasm <lucaspoletis@gmail.com>
2022-01-21 17:51:37 -0300

    feat: desafio 1

--- src/propertiesAndMethods/desafio1.kt
+++ src/propertiesAndMethods/desafio1.kt
@@ -1,4 +1,16 @@
 package propertiesAndMethods
 
-class desafio1 {
+class Course (var className: String){
+    lateinit var teacherName: String
+
+    fun setTeacher(teacher: String){
+        teacherName = teacher
+    }
+}
+
+fun main(){
+    val course = Course("Math")
+    course.setTeacher("Ms Price")
+
+    println(course.teacherName)
 }
