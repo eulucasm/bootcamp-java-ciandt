7d061a8fda8f4364b6fc0389a609b1d902130e21
eulucasm <lucaspoletis@gmail.com>
2022-01-21 17:40:11 -0300

    feat: this

--- src/propertiesAndMethods/this.kt
+++ src/propertiesAndMethods/this.kt
@@ -1,4 +1,24 @@
 package propertiesAndMethods
+class Person3 {
+    var firstName  =""
+    var child = Child()
 
-class `this` {
+    fun setFirst(firstName : String){
+        this.firstName = firstName
+    }
+
+    inner class Child{
+        var firstName = ""
+
+        fun printParentege(){
+            println("Child ${this@Child.firstName} whith parent ${this@Person3.firstName}")
+        }
+    }
+}
+
+fun main(){
+    val person = Person3()
+    person.firstName = "Sam"
+    person.child.firstName = "Susy"
+    person.child.printParentege()
 }
