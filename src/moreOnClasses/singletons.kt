ff4b6832a60ff0b134cde739fa5f08218ac7b3ef
eulucasm <lucaspoletis@gmail.com>
2022-01-21 09:19:15 -0300

    feat: Singleton

--- src/moreOnClasses/singletons.kt
+++ src/moreOnClasses/singletons.kt
@@ -1,5 +1,14 @@
 package moreOnClasses
 
+object MySingleton{
+    fun doMyStuff(data : String) {
+        println("This is my data $data")
+    }
+
+    val myConstant = "This is my Constant"
+}
+
 fun main(args: Array<String>){
+    MySingleton.doMyStuff("Hello There, " + MySingleton.myConstant)
 
 }
