651ab3c40a3b9861e2b18e125afe905a957d4106
eulucasm <lucaspoletis@gmail.com>
2022-01-21 10:52:21 -0300

    feat: challenge enum

--- src/moreOnClasses/desafio3.kt
+++ src/moreOnClasses/desafio3.kt
@@ -1,5 +1,22 @@
 package moreOnClasses
 
+enum class Animals {
+    CAT, DOG, RAT, BIRD, HAMSTER
+}
+enum class Cage{
+    SMALL, MEDIUM, LARGE
+}
+
+fun addAnimalToCage(animals: Animals, cage:Cage){
+    println("The $animals is in the $cage cage")
+}
+
 fun main(args: Array<String>){
+    addAnimalToCage(Animals.BIRD, Cage.SMALL)
+    addAnimalToCage(Animals.CAT, Cage.MEDIUM)
+    addAnimalToCage(Animals.DOG, Cage.LARGE)
+    addAnimalToCage(Animals.RAT, Cage.SMALL)
+    addAnimalToCage(Animals.HAMSTER, Cage.SMALL)
+
 
 }
