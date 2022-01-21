d55285ce738a842671322dc820a0f6c0d784d37f
eulucasm <lucaspoletis@gmail.com>
2022-01-21 18:25:02 -0300

    feat: finish session and course of -programming in kotlin

--- src/propertiesAndMethods/desafio2.kt
+++ src/propertiesAndMethods/desafio2.kt
@@ -1,4 +1,24 @@
 package propertiesAndMethods
 
-class desafio2 {
+import kotlin.properties.Delegates
+
+data class Course (var className:String){
+    val courseDescription:String by lazy {
+        "Course $className taugth by $teacherName"
+    }
+    private lateinit var teacherName : String
+
+    var room : String by Delegates.observable("no Room"){
+        property, oldValue, newValue ->("New value is $newValue")
+    }
+
+    fun setTeacher(teacher : String) {
+        teacherName = teacher
+    }
+}
+
+fun main(args: Array<String>){
+    val course = Course("Math")
+    course.setTeacher("Ms price")
+    course.room = "Library"
 }
